793c53273fb11c29b70b4b3f38df4c79ab800c41
Diyabla <bentsilnhyira@gmail.com>
2025-11-08 20:34:18 +0000

    Second commit

---
+++ python_instruction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 20
+number2= 50
+
+sum_of_numbers = number1+number2
+difference_of_numbers = number1-number2
+product_of_numbers = number1*number2
+
+
+print("The sum of ",number1,"and ",number2, " is", sum_of_numbers)
+
+print("The difference between ",number1,"and ",number2, " is", difference_of_numbers)
+
+print("The product of ",number1,"and ",number2, " is", product_of_numbers)

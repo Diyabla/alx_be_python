5d1646d943990971de950ea2df0cb0edc470cac0
Diyabla <bentsilnhyira@gmail.com>
2025-11-16 17:54:15 +0000

    corrected inputs for daily reminder application

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
-task = input('Enter a the task: ')
-priority = input('Enter the tasks priority (high,medium,low): ')
-time_bound = input('Is the task time bound? (yes/no): ')
+task = input('Enter your tasks: ')
+priority = input('Priority (high,medium,low): ')
+time_bound = input('Is it time bound? (yes/no): ')
 
 match priority:
     

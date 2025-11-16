a7231691e40c23796c44d18bd446287476302f7f
Diyabla <bentsilnhyira@gmail.com>
2025-11-16 17:58:12 +0000

    Added Reminder to print statment

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,27 +2,20 @@ task = input("Enter your task: ")
 time_bound = input("Is it time-bound? (yes/no): ")
 priority = input("Priority (high/medium/low): ")
 
+print("Reminder:")
+
 match priority:
-    
-        
     case 'high':
-        print(f'{task} ias a high priority task. Consider completing it soon!!')
-
+        print(f"{task} is a high priority task. Consider completing it soon.")
         if time_bound == 'yes':
-            print(f'{task} is a high priority task with a dealine, please remember the deadline and complete it soon')
-
+            print(f"{task} is a high priority task with a deadline. Please remember the deadline and complete it soon.")
 
     case 'medium':
-        print(f'{task} ias a medium priority task. Consider completing it when you can!!')
-
+        print(f"{task} is a medium priority task. Consider completing it when you can.")
         if time_bound == 'yes':
-            print(f'{task} is a medium priority task with a dealine, please remember the deadline and complete it')
-
+            print(f"{task} is a medium priority task with a deadline. Please remember the deadline and complete it.")
 
     case 'low':
-        print(f'{task} ias a low priority task. Consider completing it when you are free!!')
-
+        print(f"{task} is a low priority task. Consider completing it when you are free.")
         if time_bound == 'yes':
-            print(f'{task} is a low priority task with a dealine, please remember the deadline and complete it when you have free time')
-
-    
+            print(f"{task} is a low priority task with a deadline. Please remember the deadline and complete it when you have free time.")

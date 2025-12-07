8aff785cd881c340b4eacd52d656b95b34a80fd8
Diyabla <bentsilnhyira@gmail.com>
2025-12-07 19:24:31 +0000

    Correct Ebook class in library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,13 +4,13 @@ class Book:
         self.author = author
 
 
-class Ebook(Book):
+class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
     def __str__(self):
-        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
